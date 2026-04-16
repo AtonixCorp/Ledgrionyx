91065f91b64c075f405757727601df583cde4fbf
atonixdev <devatonix@gmail.com>
2026-04-15 18:08:16 -0700

    commit 9a1b2c3d4e5f67890abcdef1234567890abcdef

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import FooterPrimary from './FooterPrimary';
-import FooterSecondary from './FooterSecondary';
+import FooterPrimary from '../footer/FooterPrimary';
+import FooterSecondary from '../footer/FooterSecondary';
 import './Footer.css';
 
 const Footer = () => {
